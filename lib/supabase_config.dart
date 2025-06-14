30f1b19e136063eedb989d765f613e405ba47eae
Kiruthika M <101490793+keerthi1519@users.noreply.github.com>
2025-06-14 20:10:26 +0530

    Update supabase_config.dart

--- lib/supabase_config.dart
+++ lib/supabase_config.dart
@@ -9,8 +9,8 @@ class SupabaseConfig {
   // It's also good practice to provide a method for initializing Supabase (useful in main.dart)
   static Future<void> initialize() async {
     await Supabase.initialize(
-      url: 'https://lcobvndzdtpgdwywjtxb.supabase.co', // Replace with your Supabase URL
-      anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imxjb2J2bmR6ZHRwZ2R3eXdqdHhiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDMyMTE2OTUsImV4cCI6MjA1ODc4NzY5NX0.eosLwIg_fB2HsPFiFPZ4VPQur2ZOdrxuhDTPOr_8hTs', // Replace with your Supabase anon key
+      url: 'https://your-project.supabase.co', // Replace with your Supabase URL
+      anonKey: 'your-secret-key', // Replace with your Supabase anon key
     );
   }
 }
